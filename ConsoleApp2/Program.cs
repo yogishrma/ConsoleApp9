c61ec824a95d633632502b709fbfc92da60e9a35
visualstudio <visualstudio@122-PC40>
2019-03-06 10:15:03 -0500

    lab 4

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,30 +2,40 @@
 
 namespace DelegatesAndEvents
 {
+
     public class DelegateExercises
     {
-        public delegate void MyDelegate();
-        void Method1()
+        public delegate int MyDelegate(int intValue);
+
+        public int Method1(int intMethod1)
         {
-            Console.WriteLine("Method1");
-            Console.ReadLine();
-        }
-        public void Method2()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            return intMethod1 * 2;
         }
 
+        public int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
+        }
     }
-}
-namespace DelegatesAndEvents
-{
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method2();
+            delegateExercises.Method3();
+            Console.ReadLine();
+
         }
     }
 }
