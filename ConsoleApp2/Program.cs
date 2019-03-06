9f4db4b0958a42930da83ceeb221ac3d3e13f9a1
visualstudio <visualstudio@122-PC40>
2019-03-06 10:33:26 -0500

    Lab 12

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,34 +2,39 @@
 
 namespace DelegatesAndEvents
 {
-    public class Program
+    using System;
+
+    delegate void ExampleDelegate(string xyz);
+
+    class Program
     {
+        public static void Method1(string xyz)
+        {
+            Console.WriteLine(xyz + " Method1");
+        }
+
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
+
         public static void Main()
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
             Console.ReadLine();
         }
     }
-
-    public delegate void MyDelegate();
-
-    public class DelegateExercises
-    {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-
-
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
-        }
-    }
-
-
 }
